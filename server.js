c9f3a9486eaa32fc4764b23315179bc60093ef5a
Abbas Rouhi <rouhi438@gmail.com>
2026-02-10 12:21:49 +0100

    Add backend code, create cards table, and test with Postman

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import Database from "better-sqlite3";
+import cors from "cors";
 const app = express();
+app.use(cors());
+app.use("/assets", express.static("assets"));
 const port = 3000;
+const db = new Database("./data/cards-data.sqlite");
 
+app.get("/cards", (req, res) => {
+  const rows = db.prepare("SELECT * FROM cards").all();
+  res.json(rows);
+});
+
+app.get("/cards/random-pack", (req, res) => {
+  const packNumber = Math.floor(Math.random() * 3) + 1;
+  const rows = db.prepare("SELECT * FROM cards WHERE pack = ?").all(packNumber);
+  res.json({ pack: packNumber, cards: rows });
+});
 app.listen(port, function () {
   console.log(`> Ready on http://localhost:${port}`);
 });
